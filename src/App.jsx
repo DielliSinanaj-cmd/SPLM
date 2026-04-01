df68ed2b9fa4b4372da66d314308e66c4f58fb68
olti225 <ob73239@ubt-uni.net>
2026-04-02 00:01:59 +0200

    implementimi Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar"
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
  
 
 function App(){
@@ -8,6 +9,12 @@ function App(){
       <div className="flex min-h-screen bg-green-50">
         <Sidebar />
         <main className="flex-1 p-8 text-left">
+                <div className="flex-1 p-6">
+                        <Header />
+                </div>
+
+
+
           <Routes>
             <Route path="/" element={<h1 className="text-3xl font-bold text-green-950">Dashboard</h1>} />
             <Route path="/patients" element={<h1 className="text-3xl font-bold text-green-950">Patients</h1>} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+function Header(){
+ return (
+<div className="flex justify-between items-center mb-6">
+<h1 className="text-2xl font bold  text-gren-900">LabTrack Dashboard
+</h1>
+<button className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded">Log out  
+</button>
+
+</div>
+
+
+
+
+
+ );
+}
+export default Header;
+
+
+
+
