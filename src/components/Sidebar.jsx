92bf3553235ad2a8aefd9135e5a4c62a8668df31
DielliSinanaj-cmd <ds73504@ubt-uni.net>
2026-04-01 23:36:08 +0200

    Kompletimi i Sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+function Sidebar() {
+  return (
+    <div className="w-64 bg-green-900 text-white p-5 h-screen">
+      <h2 className="text-2x1 font-bold mb-8">LabTrack</h2>
+        
+        <ul className="space-y-3">
+          <li>
+          <Link to="/" className="block hover:bg-green-700 p-2 rounded">
+                Dashboard
+          </Link>
+          </li>
+          <li>
+            <Link to="/patients" className="block hover:bg-green-700 p-2 rounded">
+                Patients
+          </Link>
+          </li>
+          <li>
+            <Link to="/doctors" className="block hover:bg-green-700 p-2 rounded">
+                Doctors 
+          </Link>
+          </li>
+        </ul>
+    </div>
+  );
+}
+export default Sidebar;
